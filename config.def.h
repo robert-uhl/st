1a1de0352d6ea89f82f30c3d6d413dceb33d3e5c
Bob Uhl <buhl@zvelo.com>
2018-01-19 10:51:38 -0700

    Dark Nord theme

--- config.def.h
+++ config.def.h
@@ -83,43 +83,74 @@ char termname[] = "st-256color";
 static unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
+/* const char *colorname[] = { */
+/*     /\* 8 normal colors *\/ */
+/*     "#3f3f3f", */
+/*     "#cc9393", */
+/*     "#7f9f7f", */
+/*     "#f0dfaf", */
+/*     "#87afd7", */
+/*     "#8cd0d3", */
+/*     "#dc8cc3", */
+/*     "#dcdccc", */
+/*     /\* 8 bright colors *\/ */
+/*     "#4f4f4f", */
+/*     "#dca3a3", */
+/*     "#8fb28f", */
+/*     "#e0cf9f", */
+/*     "#94bff3", */
+/*     "#bc8383", */
+/*     "#7cb8bb", */
+/*     "#ffffef", */
+/*     [255] = 0, */
+
+/*     /\* more colors can be added after 255 to use with DefaultXX *\/ */
+/*     "#cccccc", */
+/*     "#333333", */
+/* }; */
+
+
+/* /\* */
+/*  * Default colors (colorname index) */
+/*  * foreground, background, cursor, reverse cursor */
+/*  *\/ */
+/* unsigned int defaultfg = 7; */
+/* unsigned int defaultbg = 0; */
+/* unsigned int defaultcs = 256; */
+/* unsigned int defaultrcs = 257; */
+
 const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+  "#3b4252", /*  0: nord1 */
+  "#bf616a", /*  1: nord11 */
+  "#a3be8c", /*  2: nord14 */
+  "#ebcb8b", /*  3: nord13 */
+  "#81a1c1", /*  4: nord9 */
+  "#b48ead", /*  5: nord15 */
+  "#88c0d0", /*  6: nord8 */
+  "#e5e9f0", /*  7: nord5 */
+  "#4c566a", /*  8: nord3 */
+  "#bf616a", /*  9: nord11 */
+  "#a3be8c", /* 10: nord14 */
+  "#ebcb8b", /* 11: nord13 */
+  "#81a1c1", /* 12: nord9 */
+  "#b48ead", /* 13: nord15 */
+  "#8fbcbb", /* 14: nord7 */
+  "#eceff4", /* 15: nord6 */
+  [255] = 0,
+  // defaults
+  [256] = "#d8dee9", /* 256: nord4 */
+  [257] = "#2e3440", /* 257: nord0 */
 };
 
-
 /*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-unsigned int defaultcs = 256;
-unsigned int defaultrcs = 257;
+* Default colors (colorname index)
+* foreground, background, cursor, reverse
+* cursor
+*/
+unsigned int defaultfg = 256; /* nord4 */
+unsigned int defaultbg = 257; /* nord0 */
+unsigned int defaultcs = 256; /* nord4 */
+unsigned int defaultrcs = 8;  /* nord3 */
 
 /*
  * Default shape of cursor
@@ -463,4 +494,3 @@ char ascii_printable[] =
 	" !\"#$%&'()*+,-./0123456789:;<=>?"
 	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
 	"`abcdefghijklmnopqrstuvwxyz{|}~";
-
